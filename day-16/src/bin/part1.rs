f3fb64f92c2a50e8d4a89edaad616cd0f5b03b68
Gaven Behrends <gzbehrends@gmail.com>
2023-12-16 22:47:50 -0600

    Day sixteen

---
+++ day-16/src/bin/part1.rs
@@ -0,0 +1,130 @@
+fn main() {
+    println!("{}", part1(include_str!("./input.txt")));
+}
+
+fn part1(input:&str) -> usize {
+    let mut map = Vec::new();
+    let mut energized = Vec::new();
+
+    for line in input.lines() {
+        map.push(Vec::from_iter(line.chars()));
+        
+        let mut empty = Vec::new();
+        for _ in 0..line.len() {
+            empty.push(Vec::new());
+        }
+        energized.push(empty);
+    }
+
+    energize(&map, (0,0), '>', &mut energized);
+
+    let mut sum = 0;
+
+    for line in energized {
+        for tile in line {
+            print!("{}", tile.len());
+            if tile.len() > 0 {
+                sum += 1;
+            }
+        }
+        println!("");
+    }
+    
+    sum
+}
+
+fn energize(map:&Vec<Vec<char>>, mut pos:(isize, isize), dir:char, acc:&mut Vec<Vec<Vec<char>>>) {
+    let mut effective = false;
+
+    while !((pos.0 < 0 || pos.1 < 0) || (pos.1 as usize >= map.len() || pos.0 as usize >= map[pos.1 as usize].len())) && map[pos.1 as usize][pos.0 as usize] == '.' {
+        if !acc[pos.1 as usize][pos.0 as usize].contains(&dir) {
+            acc[pos.1 as usize][pos.0 as usize].push(dir);
+            effective = true;
+        }
+
+        match dir {
+            '<' => { pos.0 -= 1 }
+            '>' => { pos.0 += 1 }
+            '^' => { pos.1 -= 1 }
+            'v' => { pos.1 += 1 }
+            _ => {}
+        }
+    }
+
+    if (pos.0 < 0 || pos.1 < 0) || (pos.1 as usize >= map.len() || pos.0 as usize >= map[pos.1 as usize].len()) {
+        return;
+    }
+
+    if !acc[pos.1 as usize][pos.0 as usize].contains(&dir) {
+        acc[pos.1 as usize][pos.0 as usize].push(dir);
+        effective = true;
+    }
+
+    if !effective {
+        return;
+    }
+
+    match map[pos.1 as usize][pos.0 as usize] {
+        '/' => {
+            match dir {
+                '>' => { energize(map, (pos.0, pos.1-1), '^', acc); }
+                '<' => { energize(map, (pos.0, pos.1+1), 'v', acc); }
+                'v' => { energize(map, (pos.0-1, pos.1), '<', acc); }
+                '^' => { energize(map, (pos.0+1, pos.1), '>', acc); }
+                _ => {}
+            }
+        }
+
+        '\\' => {
+            match dir {
+                '>' => { energize(map, (pos.0, pos.1+1), 'v', acc); }
+                '<' => { energize(map, (pos.0, pos.1-1), '^', acc); }
+                'v' => { energize(map, (pos.0+1, pos.1), '>', acc); }
+                '^' => { energize(map, (pos.0-1, pos.1), '<', acc); }
+                _ => {}
+            }
+        }
+
+        '|' => {
+            if dir == 'v' || dir == '>' || dir == '<' {
+                energize(map, (pos.0, pos.1+1), 'v', acc);
+            } 
+            
+            if dir == '^' || dir == '>' || dir == '<' {
+                energize(map, (pos.0, pos.1-1), '^', acc);
+            }
+        }
+
+        '-' => {
+            if dir == '^' || dir == 'v' || dir == '<' {
+                energize(map, (pos.0-1, pos.1), '<', acc);
+            }
+
+            if dir == '^' || dir == 'v' || dir == '>' {
+                energize(map, (pos.0+1, pos.1), '>', acc);
+            }
+        }
+
+        _ => {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+
+    #[test]
+    fn example() {
+        assert_eq!(part1(
+r#".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."#), 46);
+    }
+}
